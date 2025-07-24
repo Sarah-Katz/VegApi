3e1d56ac34b8b87caef8479f4ad199dd072fd112
Sarah-Katz <sarahkatza59@gmail.com>
2025-07-24 15:58:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/", routes);
+
+// Add a route for the root URL
+app.get("/", (req, res) => {
+    res.send("Welcome to the Vegan Recipe API!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
